94140311b57e397d7b14010cb3d1b75461f941bc
shivmatkawala <shivkumarmatkawala@gmail.com>
2025-09-17 10:10:35 +0530

    added variables

---
+++ variables.py
@@ -0,0 +1,69 @@
+# VARIABLE:-
+    # Variable is a container / reference / context
+    # to a value / Data / Info.
+    
+# RULES OF VARIABLE CREATION IN PYTHON:-
+
+    # 1) To create a variable we can use uppercase & lowercase alphabets.
+        #  A-Z ---> allowed
+        #  a-z ---> allowed
+        
+    # 2) To create a variable we can use numbers, but variabe should not start with number.
+        # 0-1 ----> allowed
+    
+    # 3) To create a variable we can not use any special charecter, except _
+        # _ allowed
+        
+    # 4) To create a variable we can not use any keyword.
+        # a word which used by python to operate internally,
+        # a word which has a special meaning for python.
+        
+# import keyword
+
+# print(keyword.kwlist)
+    
+# 'False', 'None', 'True', 'and', 'as', 'assert', 
+# 'async', 'await', 'break', 'class', 'continue', 
+# 'def', 'del', 'elif', 'else', 'except', 'finally', 
+# 'for', 'from', 'global', 'if', 'import', 'in', 'is',
+# 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise',
+# 'return', 'try', 'while', 'with', 'yield'
+
+
+
+#--------------------------------------------------
+
+# PRACTICE OF VARIABLES:-
+
+# 9para = 'military'
+# omega36 = 'Roadway to down'
+# print(omega36)
+
+
+# Python case sensetive language 
+    # A is not equal to a
+    # amol is not as same as Amol
+    
+
+NAME = 'shiv'
+print(NAME)
+print(id(NAME))
+
+print("----------------------")
+
+name = 'kumar'
+print(name)
+print(id(name))
+
+print("----------------------")
+
+Name = 'Rajiv'
+print(Name)
+print(id(Name))
+
+print("----------------------")
+
+
+NaMe = 'Sanjay'
+print(NaMe)
+print(id(NaMe))
