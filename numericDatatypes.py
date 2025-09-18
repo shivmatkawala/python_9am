77e48f0cb4e1525b51f7d211a5962739ea55ac32
shivmatkawala <shivkumarmatkawala@gmail.com>
2025-09-18 10:14:41 +0530

    numeric datatypes completed

---
+++ numericDatatypes.py
@@ -0,0 +1,69 @@
+# INTEGER :-
+    # A Number without decimal point,
+    # It can be +ve or -ve,
+    # Including 0
+    
+    # Ex: 4, 8, -23, 0, 5000, -123, 
+
+# x = 5
+# print(x)
+# print(type(x))   #<class 'int'>
+
+# print("-------------------------------")
+
+# y = 0
+# print(y)
+# print(type(y))
+
+# print("-------------------------------")
+
+# z = -23
+# print(z)
+# print(type(z))
+
+# print("-------------------------------")
+
+# NOT AN INTEGER EVEN 0 AFTER DECIMAL POINT 
+# m = -12.00
+# print(m)
+# print(type(m))
+
+
+# FLOAT :-
+    # A Number with decimal point,
+    # It can be +ve, -ve,
+
+    # Ex: 1.0, -0.0000, 123.4567, 89.123, -67.0
+    
+    
+# a = 5.0
+# print(a)
+# print(type(a))
+    
+# print("-------------------------------")
+
+# b = -0.000
+# print(b)
+# print(type(b))
+
+# print("-------------------------------")
+
+# c = -123.56
+# print(c)
+# print(type(c))
+
+# Complex :-
+    # A combination of Real Number & Imaginary Number.
+    # REAL NUMBER :- All Integers cum Floats are Real Numbers.
+    # IMAGINARY NUMBER:- √-1  --> j
+    
+    # Ex: 6+6j, -12+6j, -5-7j, 0.0+0j, 90.5-45.5j
+    
+
+p = 5+7j
+print(p)
+print(type(p))
+print(p.real)
+print(p.imag)
+
+    
