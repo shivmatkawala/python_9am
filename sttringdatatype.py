629ef92616e1f5d8afe932225f064b219d33f29d
shivmatkawala <shivkumarmatkawala@gmail.com>
2025-09-22 10:02:29 +0530

    string started

---
+++ sttringdatatype.py
@@ -0,0 +1,96 @@
+# STRING:-
+
+# What is String ?
+    # alphabetic charecters, numbers, special charecters,
+    # emojis or anything written inside quotes in python
+    # is nothing but a string.
+    
+# what is quotes ?
+    # ''   --> single quotes
+    # ""   --> double quotes
+    # ''' ''' ---> tripple single quotes
+    # """ """ ---> tripple double quotes
+    
+# why these many quotes ?
+    # In english grammer quotes are used to emphasize
+    # a particular, specific part of sentence specially
+    # adjectives and adverbs.
+    
+    # Examples:-
+        # Karishma is the most 'beautiful women' in our class. 
+        # Sai is the "strongest student" in our class.
+        # I was going to a temple, i met charan near temple,
+        # he was upset. i asked him '''How are you ?'''.
+        # He replied """I am not good"""
+        
+        
+# str1 = 'apple'
+# print(str1)
+# print(type(str1))
+
+# print("--------------------------")
+
+# str2 = "Grapes"
+# print(str2)
+# print(type(str2))
+
+# print("--------------------------")
+
+# str3 = '''Pineapple'''
+# print(str3)
+# print(type(str3))
+
+# print("--------------------------")
+
+# str4 = """Watermelon"""
+# print(str4)
+# print(type(str4))
+
+# print("--------------------------")
+
+# str5 = "Karishma is the most 'beautiful women' in our class."
+# print(str5)
+# print(type(str5))
+
+# print("--------------------------")
+
+# str6 = 'Sai is the "tallest man" in our class'
+# print(str6)
+# print(type(str6))
+
+# print("--------------------------")
+
+# str7 = '''Hello dear friend 'tejes', I have been searching for you since "ages".'''
+# print(str7)
+# print(type(str7))
+
+# print("--------------------------")
+
+# str8 = """'''Woww..! This is awesome. I have been waiting for this.''' rama replied """
+# print(str8)
+# print(type(str8))
+################ INDEXING ###########################
+
+# str11 = 'APPLE'
+
+# Positive Indexing
+# print(str11[0])
+# print(str11[1])
+# print(str11[2])
+# print(str11[3])
+# print(str11[4])
+
+# Negative Indexing
+# print(str11[-5])
+# print(str11[-4])
+# print(str11[-3])
+# print(str11[-2])
+# print(str11[-1])
+
+
+####################### SLICING ##############################
+str12 = "PINEAPPLE"
+print(str12[0:4:1])
+
+################ STRING IN BUILT METHODS ###############
+
