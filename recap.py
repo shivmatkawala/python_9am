f6fc88916c0326b310028f39c1f9eb9bb311b163
shivmatkawala <shivkumarmatkawala@gmail.com>
2025-09-26 10:01:43 +0530

    string methods

---
+++ recap.py
@@ -0,0 +1,109 @@
+# What is python ?
+    # Python is General purpose, High Level, Hybrid,
+    # English like, dynamically typed, case sensetive,
+    # loosely coupled programming language, which supports
+    # multiple programming paradigms like procedural,
+    # functional, object oriented, modular.
+
+# variable ?
+    # Variable is a reference to information / data
+    # variables are like containers which accomodate
+    # values / data.
+    
+# x = 5
+# y = 2.5
+# name = 'Javed'
+# village = 'Suryapet'
+# age = 20
+# wife = 'Sunanda'
+# daughter = 'Kamala'
+    
+# print(village)
+# print(age)
+# print(wife)
+
+    # RULES TO CREATE VRIABLE:-
+        # 1) To create variable can use uppercase & lowercase letters.
+                # EX: A, B, C...., a, b, c, d.....
+                
+        # 2) To create variable can use numbers but nnot at start of the variable.
+                # EX: 0, 1, 2, 3....
+                # alpha23 = 'Batch',  -->correct
+                # ban123desh = 'bangladesh'  -->correct
+                # 36omega = 'medicine' -->incorrect
+        
+        # 3) To create a varible can not use any special charecter except _
+                # Ex: man_hood = 'Robinhood',
+                # Ex: power_star = 'Pavan Kalyan',
+                # Ex: jai_balayya = 'NTB'
+                # Ex: _greet = 'Hello'
+                # Ex: name_ = 'bindu'
+        # 4) To create a variable do not use keywords
+                # 'False', 'None', 'True', 'and', 'as', 'assert', 
+                # 'async', 'await', 'break', 'class', 'continue', 
+                # 'def', 'del', 'elif', 'else', 'except', 'finally', 
+                # 'for', 'from', 'global', 'if', 'import', 'in', 'is',
+                # 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise',
+                # 'return', 'try', 'while', 'with', 'yield'
+            # EX: while = 'honda city'   --> incorrect
+            
+# data types ?
+    # Numeric Datatypes :-
+        # 1) integer  ---> int
+            # A number without decimal point is an integer
+            # An integer can be +ve or -ve
+            # Including 0
+
+            # Ex: 2, 3, -56, -0
+
+# x = -0
+# print(x)
+# print(type(-0))
+
+        
+        # 2) float    ---> float
+            # A number with decimal point is float
+            # a float can be +ve or -ve
+            
+            # Ex: -1.0, -0.0, -5.5, 34.00000000001
+            
+            
+        # 3) complex  ---> complex
+            # Combination of real number and imaginary number
+            # real number ---> all ints & all foats
+            # imaginary number --> a number which cant have a real value --> √-1  --> j
+            # Ex: 6+6j, 5+5j, 3+6j, -7.5-67j
+                 
+    # Text datatypes:-
+        # 1) string:-
+            # can create a string inside quotes
+            # '',   "",   ''' ''', """ """
+            # Ex: 'amul', "venilla", '''Hello students, how are you ?''', """David Convey is a great 'batsman' from newzeland"""
+            
+# str1 = 'amul'
+# print(str1)
+# print(type(str1))
+ 
+ 
+ 
+# str2 = "Vanila"
+# print(str2)
+# print(type(str2))
+
+
+# str3 = '''Hello 'Bujji', how are you ?'''
+# print(str3)
+# print(type(str3))
+
+
+# Indexing:-
+# x = 'apple'
+# print(x[3])
+# print(x[-2])
+
+# slicing:
+# [start Index: end Index: step]
+
+m = 'Himalaya Shampoo. !,'
+# Shampoo
+# print(m[9:16:1])
--- sttringdatatype.py
+++ sttringdatatype.py
@@ -89,8 +89,85 @@
 
 
 ####################### SLICING ##############################
-str12 = "PINEAPPLE"
-print(str12[0:4:1])
+# str12 = "PINEAPPLE"
+# print(str12[0:4:1])
 
 ################ STRING IN BUILT METHODS ###############
+
+# CASE CONVERSION METHODS:-
+
+# str1 = "apple"    # lowecase
+# str2 = "Amma@123"
+# str3 = 'PYTHON'
+# # .upper()
+# print(str1.upper())  #APPLE
+# print(str2.upper())  #AMMA@123
+# print(str3.upper())  #PYTHON
+
+# str1 = "APPLE"
+# str2 = 'ANDYshandy@123'
+# str3 = 'power'
+# # .lower()
+# print(str1.lower())  #apple
+# print(str2.lower())  #andyshandy@123
+# print(str3.lower())  #power
+
+# str1 = 'i am a diso dancer.'
+# str2 = 'i AM a DiscO DANcer'
+# str3 = 'you Stay aT road 23@downtown'
+# # .capitalize()
+# print(str1.capitalize())  #I am a diso dancer.
+# print(str2.capitalize())  #I am a disco dancer.
+# print(str3.capitalize())  #You stay at road 23@downtown
+
+# str1 = 'gama is a ray'
+# str2 = 'i HaVE 200 Ruppes'
+# str3 = 'Man Is A Man'
+
+# # .title()
+# print(str1.title())  #Gama Is A Ray
+# print(str2.title())  #I Have 200 Ruppes
+# print(str3.title())  #Man Is A Man
+
+
+# .swapcase()
+# str1 = 'APPLE grapes'
+# str2 = 'pYtHoN'
+# print(str1.swapcase())  #apple GRAPES
+# print(str2.swapcase())  #PyThOn
+
+
+# SEARCH & REPLACE METHODS:-
+
+# str1 = 'PYTHON'
+
+# # .find()
+# print(str1.find('T')) #2
+# print(str1.find('O')) #4
+# print(str1.find('y')) # -1  --> when no match found
+# print(str1.find('HO')) #3
+# print(str1.find('YH')) #-1  --> 'YH' substring not found together
+
+# str2 = 'RGvGR'
+# print(str2.find('R'))  #0
+# print(str2.find('G'))  #1
+
+
+# .rindex()
+# print(str2.rfind('R')) #4
+# print(str2.rfind('G')) #3
+
+
+# str3 = 'RGvRGvRGv'
+# print(str3.find('R')) # 0
+# print(str3.rfind('R')) # 6
+
+
+# str1 = 'Apple'
+# .index()
+
+# print(str1.index('l')) #3
+# print(str1.index('p')) #1
+# print(str1.rindex('p')) #2
+# print(str1.index('z'))  #ValueError: substring not found
 
