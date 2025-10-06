5505debff2030fe42b65b097465db818f08e5e15
shivmatkawala <shivkumarmatkawala@gmail.com>
2025-10-06 09:58:20 +0530

    list started

---
+++ listdatatype.py
@@ -0,0 +1,26 @@
+# List:-
+
+    # List is a collection datatype
+    # One variable stores / refers to Multiple values.
+    # List is created inside []
+    # Inside []  any type of data can be written. [Heterogeneous]
+    # List datatype is mutable datatype
+
+
+# Intialize List:-
+
+students = ["Shakti", "Navitha", "Balkrishna"]
+print(students)
+print(type(students))   #<class 'list'>
+
+print("--------------------------------------")
+
+students_age = [23, 21, 22, 23, 20, 24]
+print(students_age)
+print(type(students_age))
+
+print("--------------------------------------")
+
+fruits = ["Apple", "Grapes", "Watermelon", "Papaya"]
+print(fruits)
+print(type(fruits))
