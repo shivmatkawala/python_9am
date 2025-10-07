59ab40a390b67dbdac227216a04abd679700b16b
shivmatkawala <shivkumarmatkawala@gmail.com>
2025-10-07 10:06:23 +0530

    code has been addded

--- listdatatype.py
+++ listdatatype.py
@@ -5,22 +5,45 @@
     # List is created inside []
     # Inside []  any type of data can be written. [Heterogeneous]
     # List datatype is mutable datatype
-
+    # List is an Ordered collection
+    # List supports Indexing.
 
 # Intialize List:-
 
-students = ["Shakti", "Navitha", "Balkrishna"]
-print(students)
-print(type(students))   #<class 'list'>
+# students = ["Shakti", "Navitha", "Balkrishna"]
+# print(students)
+# print(type(students))   #<class 'list'>
 
-print("--------------------------------------")
+# print("--------------------------------------")
 
-students_age = [23, 21, 22, 23, 20, 24]
-print(students_age)
-print(type(students_age))
+# students_age = [23, 21, 22, 23, 20, 24]
+# print(students_age)
+# print(type(students_age))
 
-print("--------------------------------------")
+# print("--------------------------------------")
 
-fruits = ["Apple", "Grapes", "Watermelon", "Papaya"]
-print(fruits)
-print(type(fruits))
+# fruits = ["Apple", "Grapes", "Watermelon", "Papaya"]
+# print(fruits)
+# print(type(fruits))
+
+
+# grocery = ['sugar', 'jaggery', 'tea powder', 'oil']
+# print(grocery)
+# print(type(grocery))
+
+print("-----------------------------------------------")
+
+random = [12, 4.5, 6-7j, 'Python', True, ['A', 'B', 2,  False]]
+print(random)
+print(type(random))
+
+print("-----------------------------------------------")
+
+# print(random[3])
+# print(random[5])
+# print(random[0])
+
+# print(random[-3])
+# print(random[-5])
+# print(random[-6])
+
